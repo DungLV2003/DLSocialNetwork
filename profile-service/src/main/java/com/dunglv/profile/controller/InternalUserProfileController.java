37a51955014d13188db5a6d56f6ce6a4d5ffe050
Le Van Dung <ledung20102003@gmail.com>
2025-07-15 23:43:42 +0700

    Create api-gateway

--- profile-service/src/main/java/com/dunglv/profile/controller/InternalUserProfileController.java
+++ profile-service/src/main/java/com/dunglv/profile/controller/InternalUserProfileController.java
@@ -6,17 +6,18 @@ import com.dunglv.profile.service.UserProfileService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-public class UserProfileController {
+public class InternalUserProfileController {
     UserProfileService userProfileService;
 
-    @GetMapping("/users/{profileId}")
-    UserProfileResponse getProfile(@PathVariable String profileId) {
-        return userProfileService.getProfile(profileId);
+    @PostMapping("/internal/users")
+    UserProfileResponse createProfile(@RequestBody ProfileCreationRequest request) {
+        return userProfileService.createProfile(request);
     }
-
 }
