611802e7a928feafdf2dbc214ec55860272b9d9c
Le Van Dung <ledung20102003@gmail.com>
2025-08-02 11:42:53 +0700

    Format Date Created Post via Map strategy

--- post-service/src/main/java/com/dunglv/post/service/PostService.java
+++ post-service/src/main/java/com/dunglv/post/service/PostService.java
@@ -25,6 +25,7 @@ import java.util.List;
 public class PostService {
     PostRepository postRepository;
     PostMapper postMapper;
+    DateTimeFormatter dateTimeFormatter;
 
     public PostResponse createPost(PostRequest request){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -50,14 +51,18 @@ public class PostService {
 
         var pageData = postRepository.findAllByUserId(userId, pageable);
 
+        var postList = pageData.getContent().stream().map(post -> {
+            var postResponse = postMapper.toPostResponse(post);
+            postResponse.setCreated(dateTimeFormatter.format(post.getCreatedDate()));
+            return postResponse;
+        }).toList();
+
         return PageResponse.<PostResponse>builder()
                 .currentPage(page)
                 .totalPages(pageData.getTotalPages())
                 .pageSize(size)
                 .totalElements(pageData.getTotalElements())
-                .data(pageData.getContent().stream()
-                        .map(postMapper::toPostResponse)
-                        .toList())
+                .data(postList)
                 .build();
     }
 }
