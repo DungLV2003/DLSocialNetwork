9ce5878f6dae14c0c5d649a73cf5428c4af4516e
Le Van Dung <ledung20102003@gmail.com>
2025-08-03 21:52:11 +0700

    Upload file image at local

---
+++ file-service/src/main/java/com.dunglv.file/service/FileService.java
@@ -0,0 +1,32 @@
+package com.dunglv.file.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
+import java.util.UUID;
+
+@Service
+public class FileService {
+    public Object uploadFile(MultipartFile file) throws IOException {
+        Path folder = Paths.get("D:\\Material FPT\\MyPersonalProject\\Image");
+        String fileExtension = StringUtils
+                .getFilenameExtension(file.getOriginalFilename());
+
+        String fileName = Objects.isNull(fileExtension)
+                ? UUID.randomUUID().toString()
+                : UUID.randomUUID() + "." + fileExtension;
+
+        Path filePath = folder.resolve(fileName).normalize().toAbsolutePath();
+
+        Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+
+        return null;
+    }
+}
