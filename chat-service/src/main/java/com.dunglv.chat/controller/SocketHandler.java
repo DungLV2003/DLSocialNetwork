fd9c1570529ff33855e481140da1403e497c489e
Le Van Dung <ledung20102003@gmail.com>
2025-09-10 18:18:14 +0700

    Init websocket

---
+++ chat-service/src/main/java/com.dunglv.chat/controller/SocketHandler.java
@@ -0,0 +1,43 @@
+package com.dunglv.chat.controller;
+
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.annotation.OnConnect;
+import com.corundumstudio.socketio.annotation.OnDisconnect;
+import com.corundumstudio.socketio.annotation.OnEvent;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
+public class SocketHandler {
+    SocketIOServer server;
+
+    @OnConnect
+    public void onConnect(SocketIOClient client) {
+        log.info("Client connected: {}", client.getSessionId());
+    }
+
+    @OnDisconnect
+    public void onDisconnect(SocketIOClient client) {
+        log.info("Client disconnected: {}", client.getSessionId());
+    }
+    @PostConstruct // This method will be called after the bean is constructed
+    public void startServer() {
+        server.start();
+        server.addListeners(this);
+        log.info("Socket.IO server started on port {}", server.getConfiguration().getPort());
+    }
+
+    @PreDestroy
+    public void stopServer() {
+        server.stop();
+        log.info("Socket.IO server stopped.");
+    }
+}
